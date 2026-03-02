751af4dff791ce9f90613812597a97d7235997d1
Shivika Chaubey <shivikachaubey@gmail.com>
2026-03-02 23:11:47 +0530

    UC8: Implemented HashMap-based pattern storage with render function

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,27 +5,8 @@ import java.util.Map;
 
 public class OOPSBannerApp {
 
-    // Static Inner Class
-    static class CharacterPattern {
-        private char character;
-        private String[] pattern;
-
-        public CharacterPattern(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
-
-        public char getCharacter() {
-            return character;
-        }
-
-        public String[] getPattern() {
-            return pattern;
-        }
-    }
-
-    // Centralized Pattern Map
-    private static Map<Character, CharacterPattern> patternMap = new HashMap<>();
+    // Centralized Character Pattern Map
+    private static Map<Character, String[]> patternMap = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -33,26 +14,13 @@ public class OOPSBannerApp {
 
         String word = "OOPS";
 
-        // Get height from any stored character (all are 7 lines)
-        int height = patternMap.get('O').getPattern().length;
-
-        for (int i = 0; i < height; i++) {
-
-            StringBuilder lineBuilder = new StringBuilder();
-
-            for (char ch : word.toCharArray()) {
-                CharacterPattern cp = patternMap.get(ch);
-                lineBuilder.append(cp.getPattern()[i]).append("   ");
-            }
-
-            System.out.println(lineBuilder.toString());
-        }
+        renderBanner(word);
     }
 
-    // Method to initialize all patterns
+    // Store all character patterns in HashMap
     private static void initializePatterns() {
 
-        patternMap.put('O', new CharacterPattern('O', new String[]{
+        patternMap.put('O', new String[]{
                 " ***** ",
                 "*     *",
                 "*     *",
@@ -60,9 +28,9 @@ public class OOPSBannerApp {
                 "*     *",
                 "*     *",
                 " ***** "
-        }));
+        });
 
-        patternMap.put('P', new CharacterPattern('P', new String[]{
+        patternMap.put('P', new String[]{
                 " ***** ",
                 "*     *",
                 "*     *",
@@ -70,9 +38,9 @@ public class OOPSBannerApp {
                 "*      ",
                 "*      ",
                 "*      "
-        }));
+        });
 
-        patternMap.put('S', new CharacterPattern('S', new String[]{
+        patternMap.put('S', new String[]{
                 " ***** ",
                 "*     *",
                 "*      ",
@@ -80,6 +48,24 @@ public class OOPSBannerApp {
                 "      *",
                 "*     *",
                 " ***** "
-        }));
+        });
+    }
+
+    // Render banner using nested loops + StringBuilder
+    private static void renderBanner(String word) {
+
+        int height = patternMap.get(word.charAt(0)).length;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder lineBuilder = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                String[] pattern = patternMap.get(ch);
+                lineBuilder.append(pattern[i]).append("   ");
+            }
+
+            System.out.println(lineBuilder.toString());
+        }
     }
 }
