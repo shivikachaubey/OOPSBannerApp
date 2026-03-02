bf139ad7d5769b3409f3b20f16d3adf7e2bfb0c5
Shivika Chaubey <shivikachaubey@gmail.com>
2026-03-02 23:08:37 +0530

    UC7: Implemented CharacterPattern class with centralized pattern map

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,58 @@
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
+    // Static Inner Class
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
 
-        String[] o = buildO();
-        String[] p = buildP();
-        String[] s = buildS();
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
 
-        // Build full banner line by line
-        for (int i = 0; i < o.length; i++) {
-            String line = String.join("   ", o[i], o[i], p[i], s[i]);
-            System.out.println(line);
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
         }
     }
 
-    // Static method for O
-    public static String[] buildO() {
-        return new String[]{
+    // Centralized Pattern Map
+    private static Map<Character, CharacterPattern> patternMap = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        initializePatterns();
+
+        String word = "OOPS";
+
+        // Get height from any stored character (all are 7 lines)
+        int height = patternMap.get('O').getPattern().length;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder lineBuilder = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                CharacterPattern cp = patternMap.get(ch);
+                lineBuilder.append(cp.getPattern()[i]).append("   ");
+            }
+
+            System.out.println(lineBuilder.toString());
+        }
+    }
+
+    // Method to initialize all patterns
+    private static void initializePatterns() {
+
+        patternMap.put('O', new CharacterPattern('O', new String[]{
                 " ***** ",
                 "*     *",
                 "*     *",
@@ -25,12 +60,9 @@ public class OOPSBannerApp {
                 "*     *",
                 "*     *",
                 " ***** "
-        };
-    }
+        }));
 
-    // Static method for P
-    public static String[] buildP() {
-        return new String[]{
+        patternMap.put('P', new CharacterPattern('P', new String[]{
                 " ***** ",
                 "*     *",
                 "*     *",
@@ -38,12 +70,9 @@ public class OOPSBannerApp {
                 "*      ",
                 "*      ",
                 "*      "
-        };
-    }
+        }));
 
-    // Static method for S
-    public static String[] buildS() {
-        return new String[]{
+        patternMap.put('S', new CharacterPattern('S', new String[]{
                 " ***** ",
                 "*     *",
                 "*      ",
@@ -51,6 +80,6 @@ public class OOPSBannerApp {
                 "      *",
                 "*     *",
                 " ***** "
-        };
+        }));
     }
 }
