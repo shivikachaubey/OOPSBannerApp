c9db729241d9d9aeaa18f1522050230cf8b651de
Shivika Chaubey <shivikachaubey@gmail.com>
2026-03-02 23:03:18 +0530

    UC6: Refactored using static helper methods for O, P, S patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,20 +4,53 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Array declaration + initialization in one statement
-        String[] banner = {
-                String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", " ***** ", " ***** ", " ***** "),
-                String.join("   ", "*     *", "*      ", "*      ", "      *"),
-                String.join("   ", "*     *", "*      ", "*      ", "      *"),
-                String.join("   ", " ***** ", "*      ", " ***** ", " ***** ")
-        };
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
 
-        // Enhanced for-loop
-        for (String line : banner) {
+        // Build full banner line by line
+        for (int i = 0; i < o.length; i++) {
+            String line = String.join("   ", o[i], o[i], p[i], s[i]);
             System.out.println(line);
         }
+    }
+
+    // Static method for O
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Static method for P
+    public static String[] buildP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Static method for S
+    public static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
     }
 }
